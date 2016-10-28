9759dcfeb30eba17e04641232ecd60520081a0ca
LBH <dlqudgml0503@naver.com>
2016-10-28 12:45:58 +0900

    SecondMax.java 1차 완료.

--- homework6/src/homework6/SecondMax.java
+++ homework6/src/homework6/SecondMax.java
@@ -1,5 +1,34 @@
 package homework6;
+import java.util.*;
 
 public class SecondMax {
+	private int[] numArray = new int[10];
+	private Scanner input = new Scanner(System.in);
 
+	public  SecondMax(){}
+	
+	public void numInput(){
+		for(int i=1;i<=10;i++){
+			System.out.print(i+"번째 수를 입력하시오");
+			numArray[i-1] = input.nextInt();
+		}
+	}
+
+	public void secondMaxChoose(){
+		int firstMax = 0;
+		int secondMax = 0;
+		for(int j=1;j<=10;j++){
+			firstMax=Math.max(numArray[i], numArray[i-0]);
+		}
+		for(int j=1;j<=10;j++){
+			if(firstMax == secondMax){
+				break;
+			}
+			secondMax=Math.max(numArray[j], numArray[j-0]);
+		}
+		}
+	public void init(){
+		numInput();
+		secondMaxChoose();
+	}
 }
